1c9625a2d4cb1e3bc2baf9134b166fa946fb840d
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-06 14:48:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/payment-sheet', async (req, res) => {
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: req.body.amount,
+            currency: 'lkr',
+            automatic_payment_methods: {
+                enabled: true
+            }
+        })
+        res.json({paymentIntent: paymentIntent.client_secret});
+    } catch (error) {
+        res.status(400).json({
+            error: error.message,
+        });
+        console.log(error)
+    }
+});
+
+app.get('/test', (req, res) => {
+    res.send('Server is reachable');
+});
+
+
+
+app.listen(PORT, () => {
+    console.log("Server is Running at " + PORT);
+});
